73bd081ee709223942e44da0b7e1d15e97650782
SHUBHAM KHAIRMODE <shubhambkhairmode@gmail.com>
2023-10-25 17:56:14 +0530

    model trainer

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import sys
 from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_ingestion import DataIngestionConfig
 from src.mlproject.components.data_transformation import DataTransformationConfig, DataTransformation
+from src.mlproject.components.model_tranier import ModelTrainerConfig, ModelTrainer
 import os
 
 
@@ -18,7 +19,11 @@ if __name__ == '__main__':
 
         #data_transformation_config = DataTransformationConfig()
         data_transformation = DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+        train_arr, test_arr,_ = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+
+        # model trainer
+        model_trainer = ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr, test_arr))
 
         
 
